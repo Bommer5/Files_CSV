2dac8a9301a1ba93723c4964ed689c2d644e283f
Bommer5 <76489146+Bommer5@users.noreply.github.com>
2024-12-17 15:51:44 +0100

    Update main.py

--- projet_CSV/main.py
+++ projet_CSV/main.py
@@ -19,9 +19,12 @@ if __name__ == "__main__":
     # Initialisation d'argparse pour gérer les options en ligne de commande
     parser = argparse.ArgumentParser(description="Application de gestion de base de données CSV.")
     parser.add_argument('--reload', action='store_true', help="Recharger la base de données avant d'exécuter le programme")
+    parser.add_argument('--help', action='store_true', help="Help")
     args = parser.parse_args()
 
     # Si l'option --reload est passée, on recharge la base de données
+    if args.help :
+        print("voir le fichier feature")
     if args.reload:
         recharger_base_de_donnees()
     print(args.reload)
